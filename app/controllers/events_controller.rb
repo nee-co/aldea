34dd93a87b474b5e60a237a37aba98034fe91a73
Tomohiko Mimura <mito.5525@gmail.com>
2016-05-27 00:26:43 +0900

    サンプルAPI: 不要なコメントを削除する

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,18 +2,15 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy]
 
   # GET /events
-  # GET /events.json
   def index
     @events = Event.all
   end
 
   # GET /events/1
-  # GET /events/1.json
   def show
   end
 
   # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
 
@@ -25,7 +22,6 @@ class EventsController < ApplicationController
   end
 
   # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     if @event.update(event_params)
       render :show, status: :ok, location: @event
@@ -35,19 +31,17 @@ class EventsController < ApplicationController
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.fetch(:event, {})
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.fetch(:event, {})
+  end
 end
