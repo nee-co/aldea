6d956f6a57dffdad934721f050add7b0cc7ab177
r_hashimoto <r.h0312m@gmail.com>
2016-06-15 12:27:17 +0900

    イベントのルーティングを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  resources :events, { format: 'json' }
+  resources :events, { format: 'json' } do
+    collection do
+      get :search
+    end
+    member do
+      put :public
+      put :join
+      post :comment
+    end
+  end
 end
