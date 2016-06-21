cbfd65541cafb8ca274d979025ce12b37477c7fb
r_hashimoto <r.h0312m@gmail.com>
2016-06-21 11:32:44 +0900

    eventsのseedを作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# Create Events
+CSV.foreach('db/seeds/events.csv') do |list|
+  Event.create(
+    title: list[0],
+    body: list[1],
+    register_id: list[2].to_i,
+    published_at: list[3],
+    started_at: list[4],
+    ended_at: list[5],
+    venue: list[6],
+    entry_upper_limit: list[7].to_i,
+    status: list[8].to_i
+  )
+end
