6aa10b8f7a12f7976c66c4d9f1b6ffa4507685b1
r_hashimoto <r.h0312m@gmail.com>
2016-07-20 10:38:13 +0900

    Eventのtitle_Like検索のscopeを追加する

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,4 +20,8 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :entries, dependent: :delete_all
   has_and_belongs_to_many :tags
+
+  scope :title_like, -> word {
+    where("title like '%" + word + "%'")
+  }
 end
