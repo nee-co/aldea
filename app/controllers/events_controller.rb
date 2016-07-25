d26f0ab9da2379894be3cda03e9108bd3af9857b
r_hashimoto <r.h0312m@gmail.com>
2016-07-25 10:33:34 +0900

    indexアクションを削除する

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy]
 
-  # GET /events
-  def index
-    @events = Event.all
-  end
-
   # GET /events/1
   def show
   end
