5a85612e3b62f5f8d4f931c646900b37f51c1744
r_hashimoto <r.h0312m@gmail.com>
2016-09-02 11:21:57 +0900

    cuentaからuserを取得するメソッドを追加する

--- services/event_user_service.rb
+++ services/event_user_service.rb
@@ -1,4 +1,13 @@
 class EventUserService
   def initialize(event)
   end
+
+  def self.list_users(event)
+    register = event.register_id
+    entries = event.entries.ids
+    comment_users = event.comments.ids
+    user_number = []
+    user_number.push(register).push(entries).push(comment_users).flatten!
+    @users = User.list(user_number.join(' '))
+  end
 end
