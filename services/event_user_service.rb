d6d1e798299f0aefe55cc64fbb89534d04b34ba4
r_hashimoto <r.h0312m@gmail.com>
2016-10-20 13:33:43 +0900

    イベントに関係するユーザを取得するメソッドを実装する

--- services/event_user_service.rb
+++ services/event_user_service.rb
@@ -3,11 +3,32 @@ class EventUserService
   end
 
   def self.list_users(event)
-    register = event.register_id
-    entries = event.entries.ids
-    comment_users = event.comments.ids
+    register_id = event.register_id
+    entries_ids = event.entries.ids
+    comment_users_ids = event.comments.pluck(:user_id)
     user_number = []
-    user_number.push(register).push(entries).push(comment_users).flatten!
-    @users = User.list(user_number.join(' '))
+    user_number.push(register_id).push(entries_ids).push(comment_users_ids).flatten!
+    users = User.list(user_number.join(' '))
+
+    register = []
+    entries = []
+    comment_users = []
+    for user in users["users"] do
+      if register_id == user.user_id then
+        register.push(user)
+      end
+      if entries_ids.include?(user.user_id) then
+        entries.push(user)
+      end
+      if comment_users_ids.include?(user.user_id) then
+        comment_users.push(user)
+      end
+    end
+    users = Hash.new
+    users.store("register",register)
+    users.store("entries",entries)
+    users.store("comment_users",comment_users)
+
+    users
   end
 end
