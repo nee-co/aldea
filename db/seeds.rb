84fa695b2818dffd648f5dc19bfa8814c732b1b7
Tomohiko Mimura <mito.5525@gmail.com>
2016-10-22 20:40:45 +0900

    seedは各環境ごとに使い分ける
    master: 全環境共通のデータ (今回はマスタデータとしてタグを登録)
    development: 開発時に使用するダミーデータ
    production/test: 使用する必要があったら随時追加すること

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'csv'
-
-# Create Events
-CSV.foreach('db/seeds/events.csv') do |list|
-  Event.create(
-    title: list[0],
-    body: list[1],
-    register_id: list[2].to_i,
-    published_at: list[3],
-    started_at: list[4],
-    ended_at: list[5],
-    venue: list[6],
-    entry_upper_limit: list[7].to_i,
-    status: list[8].to_i
-  )
+Dir.glob(Rails.root.join("db/seeds/master/*.rb")).sort.each do |path|
+  load path
 end
 
-# Create Comments
-CSV.foreach('db/seeds/comments.csv') do |list|
-  Comment.create(
-    body: list[0],
-    event_id: list[1].to_i,
-    user_id: list[2].to_i,
-    posted_at: list[3]
-  )
-end
-
-# Create Entries
-CSV.foreach('db/seeds/entries.csv') do |list|
-  Entry.create(
-    event_id: list[0].to_i,
-    user_id: list[1].to_i
-  )
-end
-
-# Create Tags
-CSV.foreach('db/seeds/tags.csv') do |list|
-  Tag.create(name: list[0])
-end
-
-# Create Events_tags
-CSV.foreach('db/seeds/events_tags.csv') do |list|
-  Event.find(list[0]).tags << Tag.find(list[1])
+Dir.glob("#{Rails.root}/db/seeds/#{Rails.env}/*.rb").sort.each do |path|
+  load path
 end
