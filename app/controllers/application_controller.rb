51e19d43c5c3afc627181f958b80c6ffc69050ee
Tomohiko Mimura <mito.5525@gmail.com>
2016-10-24 23:14:32 +0900

    ログイン中のユーザを取得できるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+  def current_user
+    if @current_user.nil? && request.env["HTTP_X_CONSUMER_CUSTOM_ID"].present?
+      @current_user = Cuenta::User.find(request.env["HTTP_X_CONSUMER_CUSTOM_ID"])
+    end
+    @current_user
+  end
 end
