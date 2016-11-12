3189472a467923769c76cdaa5c170c2ee19b6596
Tomohiko Mimura <mito.5525@gmail.com>
2016-11-13 01:56:48 +0900

    標準のmigrationに移行する

---
+++ db/migrate/20161112160612_initial_schema.rb
@@ -0,0 +1,51 @@
+class InitialSchema < ActiveRecord::Migration[5.0]
+  def change
+    create_table "tags", comment: "タグ" do |t|
+      t.string "name", null: false, comment: "タグ名"
+      t.timestamps
+    end
+
+    create_table "events", comment: "イベント" do |t|
+      t.string "title", null: false, comment: "タイトル"
+      t.text "body", comment: "内容"
+      t.integer "register_id", null: false, comment: "登録ユーザーID"
+      t.timestamp "published_at", null: true, comment: "公開日時"
+      t.timestamp "started_at", null: true, comment: "開始日時"
+      t.timestamp "ended_at", null: true, comment: "終了日時"
+      t.string "venue", null: true, comment: "会場"
+      t.integer "entry_upper_limit", null: true, comment: "人数上限"
+      t.integer "status", null: false, default: 0, comment: "ステータス(0:非公開(下書き), 1:公開/受付中, 2:満員, 3:受付終了)"
+      t.string "image", comment: "イベント画像"
+      t.timestamps
+    end
+
+    create_table "comments", comment: "コメント" do |t|
+      t.text "body", null: false, comment: "コメント内容"
+      t.references :event, null: false, comment: "イベントID"
+      t.references :user, null: false, comment: "ユーザID"
+      t.timestamp "posted_at", null: false, comment: "投稿日時"
+      t.timestamps
+
+      t.foreign_key :events, column: "event_id", name: "comments_event_id_fk"
+      t.foreign_key :events, column: "user_id", name: "comments_user_id_fk"
+    end
+
+    create_table "entries", comment: "参加" do |t|
+      t.references :event, null: false, comment: "イベントID"
+      t.references :user, null: false, comment: "ユーザID"
+      t.timestamps
+
+      t.index %i(event_id user_id), name: "index_entries_on_event_id_user_id", unique: true
+      t.foreign_key :events, column: "event_id", name: "entries_event_id_fk"
+    end
+
+    create_table "events_tags", comment: "イベント-タグ" do |t|
+      t.integer "event_id", null: false, comment: "イベントID"
+      t.integer "tag_id", null: false, comment: "タグID"
+
+      t.index %i(event_id tag_id), name: "index_events_tags_on_event_id_tag_id", unique: true
+      t.foreign_key :tags, column: "tag_id", name: "events_tags_tag_id_fk"
+      t.foreign_key :events, column: "event_id", name: "events_tags_event_id_fk"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161112160612) do
 
-  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", comment: "コメント" do |t|
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "コメント" do |t|
     t.text     "body",       limit: 65535, null: false, comment: "コメント内容"
     t.integer  "event_id",                 null: false, comment: "イベントID"
-    t.integer  "user_id",                  null: false, comment: "ユーザーID"
+    t.integer  "user_id",                  null: false, comment: "ユーザID"
     t.datetime "posted_at",                null: false, comment: "投稿日時"
-    t.datetime "created_at",                            comment: "レコード作成日時"
-    t.datetime "updated_at",                            comment: "レコード更新日時"
-    t.index ["event_id"], name: "comments_event_id_fk", using: :btree
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["event_id"], name: "index_comments_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  create_table "entries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", comment: "参加" do |t|
+  create_table "entries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "参加" do |t|
     t.integer  "event_id",   null: false, comment: "イベントID"
-    t.integer  "user_id",    null: false, comment: "ユーザーID"
-    t.datetime "created_at",              comment: "レコード作成日時"
-    t.datetime "updated_at",              comment: "レコード更新日時"
+    t.integer  "user_id",    null: false, comment: "ユーザID"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["event_id", "user_id"], name: "index_entries_on_event_id_user_id", unique: true, using: :btree
+    t.index ["event_id"], name: "index_entries_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_entries_on_user_id", using: :btree
   end
 
-  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", comment: "イベント" do |t|
+  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "イベント" do |t|
     t.string   "title",                                       null: false, comment: "タイトル"
-    t.text     "body",              limit: 65535,             null: false, comment: "内容"
+    t.text     "body",              limit: 65535,                          comment: "内容"
     t.integer  "register_id",                                 null: false, comment: "登録ユーザーID"
     t.datetime "published_at",                                             comment: "公開日時"
     t.datetime "started_at",                                               comment: "開始日時"
@@ -40,24 +43,25 @@ ActiveRecord::Schema.define(version: 0) do
     t.string   "venue",                                                    comment: "会場"
     t.integer  "entry_upper_limit",                                        comment: "人数上限"
     t.integer  "status",                          default: 0, null: false, comment: "ステータス(0:非公開(下書き), 1:公開/受付中, 2:満員, 3:受付終了)"
-    t.string   "image",                                       null: false, comment: "イベント画像"
-    t.datetime "created_at",                                               comment: "レコード作成日時"
-    t.datetime "updated_at",                                               comment: "レコード更新日時"
+    t.string   "image",                                                    comment: "イベント画像"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
   end
 
-  create_table "events_tags", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", comment: "イベント-タグ" do |t|
+  create_table "events_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "イベント-タグ" do |t|
     t.integer "event_id", null: false, comment: "イベントID"
     t.integer "tag_id",   null: false, comment: "タグID"
     t.index ["event_id", "tag_id"], name: "index_events_tags_on_event_id_tag_id", unique: true, using: :btree
     t.index ["tag_id"], name: "events_tags_tag_id_fk", using: :btree
   end
 
-  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", comment: "タグ" do |t|
+  create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "タグ" do |t|
     t.string   "name",       null: false, comment: "タグ名"
-    t.datetime "created_at",              comment: "レコード作成日時"
-    t.datetime "updated_at",              comment: "レコード更新日時"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "events", column: "user_id", name: "comments_user_id_fk"
   add_foreign_key "comments", "events", name: "comments_event_id_fk"
   add_foreign_key "entries", "events", name: "entries_event_id_fk"
   add_foreign_key "events_tags", "events", name: "events_tags_event_id_fk"
