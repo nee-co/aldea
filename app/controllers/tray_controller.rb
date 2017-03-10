4a2c386ace0b8a104d90f5cb72ff5378f8ab49db
Yuya Otsuka <otsuka.main@google.com>
2017-03-10 10:19:27 +0900

    Revert "[add] total_countを返却"
    This reverts commit 27a84adda0c90e931359692d02963e56994fa401.

--- app/controllers/tray_controller.rb
+++ app/controllers/tray_controller.rb
@@ -3,13 +3,11 @@ class TrayController < ApplicationController
 
   def entries
     @elements = current_user.entry_events.yet.order(:start_date).limit(@limit).offset(@offset).decorate
-    @total_count = @elements.object.total_count
     render "elements"
   end
 
   def own
     @elements = current_user.owned_events.yet.order(:start_date).limit(@limit).offset(@offset).decorate
-    @total_count = @elements.object.total_count
     render "elements"
   end
 end
--- app/views/tray/elements.json.jbuilder
+++ app/views/tray/elements.json.jbuilder
@@ -1,4 +1,3 @@
-json.total_count @total_count
 json.elements(@elements) do |element|
   json.extract! element, *%i(id type title image)
   json.meta element.meta
